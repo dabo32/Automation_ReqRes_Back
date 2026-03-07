f2cbf3cd698e88cd21077736d83afc6589a883c9
David Santiago <dasabo30@gmail.com>
2026-03-07 12:38:47 -0500

    feat: implement ReqResEndpoints utility for URI management

---
+++ src/main/java/co/com/screenplay/project/userinterfaces/ReqResEndpoints.java
@@ -0,0 +1,8 @@
+package co.com.screenplay.project.userinterfaces;
+
+public class ReqResEndpoints {
+    public static final String LIST_USERS = "/users?page=2";
+    public static final String CREATE_USER = "/users";
+    public static final String UPDATE_USER = "/users/2";
+    public static final String DELETE_USER = "/users/2";
+}
