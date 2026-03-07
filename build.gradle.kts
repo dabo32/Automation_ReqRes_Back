2d59b64f465ee12aaf2ebc3a4f63fcf03953e258
David Santiago <dasabo30@gmail.com>
2026-03-07 12:28:02 -0500

    chore: setup project baseline and serenity configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("net.serenity-bdd.serenity-gradle-plugin") version "4.0.15"
+    id("net.serenity-bdd.serenity-gradle-plugin") version "4.2.8"
 }
 
 group = "org.bohorquez.automation"
@@ -10,39 +10,55 @@ repositories {
     mavenCentral()
 }
 
-val serenityVersion = "4.0.15"
-val cucumberVersion = "7.15.0"
-val junitVersion = "5.10.2"
+val serenityVersion = "4.2.8"
+val cucumberVersion = "7.20.1"
+val junitVersion = "5.11.3"
+val restAssuredVersion = "5.5.0"
+val lombokVersion = "1.18.34"
 
 dependencies {
     // JUnit 5
     testImplementation(platform("org.junit:junit-bom:$junitVersion"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.platform:junit-platform-suite-api")
+    testRuntimeOnly("org.junit.platform:junit-platform-suite-engine")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-    // Cucumber
+    // Cucumber & Serenity Integration
     testImplementation("io.cucumber:cucumber-java:$cucumberVersion")
-    testImplementation("io.cucumber:cucumber-junit:$cucumberVersion")
-
-    // Serenity Core & Screenplay
-    testImplementation("net.serenity-bdd:serenity-core:$serenityVersion")
+    testImplementation("io.cucumber:cucumber-junit-platform-engine:$cucumberVersion")
     testImplementation("net.serenity-bdd:serenity-cucumber:$serenityVersion")
-    testImplementation("net.serenity-bdd:serenity-screenplay:$serenityVersion")
-    testImplementation("net.serenity-bdd:serenity-screenplay-webdriver:$serenityVersion")
 
-    // 3. Agregar librería específica para Screenplay + REST (Si vas a usar ReqRes)
-    testImplementation("net.serenity-bdd:serenity-screenplay-rest:$serenityVersion")
+    // Serenity & REST
+    implementation("net.serenity-bdd:serenity-core:$serenityVersion")
+    implementation("net.serenity-bdd:serenity-screenplay:$serenityVersion")
+    implementation("net.serenity-bdd:serenity-screenplay-rest:$serenityVersion")
+    implementation("io.rest-assured:rest-assured:$restAssuredVersion")
 
-    // Rest Assured
-    testImplementation("io.rest-assured:rest-assured:5.3.2")
+    // Lombok
+    compileOnly("org.projectlombok:lombok:$lombokVersion")
+    annotationProcessor("org.projectlombok:lombok:$lombokVersion")
+    testCompileOnly("org.projectlombok:lombok:$lombokVersion")
+    testAnnotationProcessor("org.projectlombok:lombok:$lombokVersion")
 }
 
 tasks.test {
     useJUnitPlatform()
-    systemProperties(System.getProperties().mapKeys { it.key.toString() })
+    systemProperty("cucumber.filter.tags", System.getProperty("cucumber.filter.tags"))
+    systemProperty("cucumber.plugin", "io.cucumber.core.plugin.SerenityReporterParallel,pretty")
+
+    filter {
+        includeTestsMatching("*Runner")
+    }
+
+    testLogging {
+        showStandardStreams = true
+        events("passed", "skipped", "failed")
+    }
 }
 
 serenity {
-    setTestRoot("src/test/resources/features")
+    testRoot = "co.com.screenplay.project"
+    requirementsBaseDir = "src/test/resources/features"
     reports = listOf("html")
 }
